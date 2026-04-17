e0462aca49ba3b3a3664ff00bac5772216186b68
Manha Cetcc <Manha.Cetcc@labfies.org.br>
2026-04-17 09:10:23 -0300

    Initializating files for spring

---
+++ src/test/java/com/imposto/adm/AdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imposto.adm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
