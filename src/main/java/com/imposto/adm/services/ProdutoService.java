6ce36a7905b207da9e3ec5a0f7fe2b0c97193d2b
Miguel Lima <mglima1279@gmail.com>
2026-04-17 10:13:57 -0300

    Service functionallity working

---
+++ src/main/java/com/imposto/adm/services/ProdutoService.java
@@ -0,0 +1,56 @@
+package com.imposto.adm.services;
+
+import java.math.BigDecimal;
+
+import org.springframework.stereotype.Service;
+
+import com.imposto.adm.dto.ProdutoRequest;
+import com.imposto.adm.entities.Produto;
+import com.imposto.adm.repositories.ProdutoRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProdutoService {
+    private final ProdutoRepository produtoRepository;
+
+    public Produto create(ProdutoRequest request) {
+        Produto produto = request.toProduto();
+
+        calcValues(produto);
+
+        return produtoRepository.save(produto);
+    }
+
+    public Produto getById(Long id) {
+        return produtoRepository.findById(id).orElseThrow(() -> new RuntimeException("Produto não encontrado"));
+    }
+
+    public Produto updateById(Long id, ProdutoRequest request) {
+        Produto produto = getById(id);
+        
+        produto.setQtd(request.getQtd());
+
+        calcValues(produto);
+        
+        return produtoRepository.save(produto);
+    }
+
+    public void deleteById(Long id) {
+        produtoRepository.deleteById(id);
+    }
+
+    private void calcValues(Produto produto) {
+        produto.setValorTotal(produto.getValorUnd().multiply(BigDecimal.valueOf(produto.getQtd())));
+        switch (produto.getTipo()) {
+            case "1" -> produto.setValorImposto(BigDecimal.ZERO);
+            case "2" -> produto.setValorImposto(produto.getValorTotal().multiply(BigDecimal.valueOf(0.08)));
+            case "3" -> produto.setValorImposto(produto.getValorTotal().multiply(BigDecimal.valueOf(0.10)));
+            case "4" -> produto.setValorImposto(produto.getValorTotal().multiply(BigDecimal.valueOf(0.12)));
+            case "5" -> produto.setValorImposto(produto.getValorTotal().multiply(BigDecimal.valueOf(0.17)));
+            default -> throw new IllegalArgumentException("Tipo de produto inválido: " + produto.getTipo());
+        }
+        produto.setValorFinal(produto.getValorTotal().add(produto.getValorImposto()));
+    }
+}
