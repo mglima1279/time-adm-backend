c598e50eff1ad4a0aaea6635f6365e9b81858253
Manha Cetcc <Manha.Cetcc@labfies.org.br>
2026-04-17 09:27:45 -0300

    Criação da entidade produto no backend

--- src/main/java/com/imposto/adm/entities/Produto.java
+++ src/main/java/com/imposto/adm/entities/Produto.java
@@ -1,6 +1,12 @@
 package com.imposto.adm.entities;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,5 +18,34 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Produto {
-    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255, name = "nome")
+    private String nome;
+
+    @Column(length = 255, name = "caracteristicas")
+    private String caracteristicas;
+
+    @Column(nullable = false, precision = 10, scale = 2, name = "valor_und")
+    private BigDecimal valorUnd;
+
+    @Column(nullable = false, length = 10, name = "und")
+    private String und;
+
+    @Column(nullable = false, length = 10, name = "tipo")
+    private String tipo;
+
+    @Column(nullable = false, precision = 10, scale = 3, name = "qtd")
+    private Double qtd;
+
+    @Column(nullable = false, precision = 12, scale = 2, name = "valor_total")
+    private BigDecimal valorTotal;
+
+    @Column(nullable = false, precision = 12, scale = 2, name = "valor_imposto")
+    private BigDecimal valorImposto;
+
+    @Column(nullable = false, precision = 12, scale = 2, name = "valor_final")
+    private BigDecimal valorFinal;
 }
